2a6e648dd331a4dd23b5d6f713de512b31416180
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2023-01-24 21:45:32 +0700

    Update AuthenticationController.java

--- src/main/java/com/social/socialnetwork/Controller/AuthenticationController.java
+++ src/main/java/com/social/socialnetwork/Controller/AuthenticationController.java
@@ -4,6 +4,9 @@ import com.social.socialnetwork.Service.AuthenticationService;
 import com.social.socialnetwork.dto.AuthenticationReqest;
 import com.social.socialnetwork.dto.AuthenticationResponse;
 import com.social.socialnetwork.dto.RegisterReqest;
+import com.social.socialnetwork.dto.ResponseDTO;
+import com.social.socialnetwork.exception.AppException;
+import com.social.socialnetwork.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,18 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final UserRepository userRepository;
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<?> register(
             @RequestBody RegisterReqest request
     )
-    {
+    {   if(!userRepository.findByEmail(request.getEmail()).isPresent())
         return ResponseEntity.ok(authenticationService.register(request));
+        else return  ResponseEntity.ok(new ResponseDTO(false,"Email is Exists",null));
     }
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public ResponseEntity<?> authenticate(
             @RequestBody AuthenticationReqest request
     )
     {
+        if(userRepository.findByEmail(request.getEmail()).isPresent())
         return ResponseEntity.ok(authenticationService.authenticate(request));
+        else return  ResponseEntity.ok("Email is not Exists");
     }
 }
