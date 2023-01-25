3a80e9de1a702d3d60f36e46058de48e0a860499
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2023-01-25 21:49:32 +0700

    update repouser

--- src/main/java/com/social/socialnetwork/repository/UserRepository.java
+++ src/main/java/com/social/socialnetwork/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.social.socialnetwork.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import java.util.Optional;
 @Repository
 public interface  UserRepository  extends JpaRepository<User, Long> {
