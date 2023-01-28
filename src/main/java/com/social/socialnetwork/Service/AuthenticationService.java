7d97ea4aac2e943076b8f74e9cfdf9e68fbf0e99
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2023-01-28 21:19:56 +0700

    Revert "Revert "update verify email""
    This reverts commit 028ce49d2c8655aa2ba46668afe8d32d059bc1e2.

--- src/main/java/com/social/socialnetwork/Service/AuthenticationService.java
+++ src/main/java/com/social/socialnetwork/Service/AuthenticationService.java
@@ -5,7 +5,9 @@ import com.social.socialnetwork.dto.AuthenticationReqest;
 import com.social.socialnetwork.dto.AuthenticationResponse;
 import com.social.socialnetwork.dto.RegisterReqest;
 import com.social.socialnetwork.exception.AppException;
+import com.social.socialnetwork.model.ConfirmationToken;
 import com.social.socialnetwork.model.Role;
+import com.social.socialnetwork.repository.ConfirmationTokenRepository;
 import com.social.socialnetwork.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.validator.GenericValidator;
@@ -16,6 +18,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
@@ -23,6 +27,7 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
     public AuthenticationResponse register(RegisterReqest request) {
         if (!GenericValidator.isEmail(request.getEmail()))
             throw new AppException(400, "Wrong email");
@@ -57,6 +62,37 @@ public class AuthenticationService {
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken).build();
+    }
+
+    public void saveVerificationTokenForUser(User users, String token) {
+        ConfirmationToken  verificationToken = new ConfirmationToken(users, token);
+        System.out.println(verificationToken.getExpirationTime());
+        confirmationTokenRepository.save(verificationToken);
+    }
+    public String validateVerificationToken(String token, String email) {
+        ConfirmationToken verificationToken
+                = confirmationTokenRepository.findVerificationTokenByTokenAndUser_Email(token, email);
+
+        if (verificationToken == null) {
+            return "invalid";
+        }
+
+        User user = verificationToken.getUser();
+        Calendar cal = Calendar.getInstance();
+
+        if ((verificationToken.getExpirationTime().getTime()
+                - cal.getTime().getTime()) <= 0) {
+            confirmationTokenRepository.delete(verificationToken);
+            return "expired";
+        }
+        verificationToken.setToken(null);
+        confirmationTokenRepository.save(verificationToken);
+        user.setEnabled(true);
+        user.setRole(Role.USER);
+        userRepository.save(user);
+
+        return "valid";
+
     }
 
 }
